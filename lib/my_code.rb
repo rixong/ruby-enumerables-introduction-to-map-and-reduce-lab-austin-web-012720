9387d574702afcecc2a6fb46449518352f1ca0c5
Rick Glascock <rixong@gmail.com>
2020-01-19 00:32:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,15 +10,15 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-source_array)
+source_array.map { |ele| ele * 2 }
 end
 
 def map_to_square(source_array)
-source_array
+source_array.map { |ele| ele*ele }
 end
 
 def reduce_to_total(source_array, total = 0)
-source_array
+  source_array (:+)
 end
 
 def reduce_to_all_true (source_array)
