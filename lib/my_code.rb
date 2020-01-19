54c04233339f9ad88de3e4d6ea166011c3e672fb
Rick Glascock <rixong@gmail.com>
2020-01-19 00:31:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,23 +6,23 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
- 
+ source_array.map
 end
 
 def map_to_double(source_array)
-
+source_array)
 end
 
 def map_to_square(source_array)
-
+source_array
 end
 
 def reduce_to_total(source_array, total = 0)
-
+source_array
 end
 
 def reduce_to_all_true (source_array)
-
+source_array
 end
 
 def reduce_to_any_true (source_array)
