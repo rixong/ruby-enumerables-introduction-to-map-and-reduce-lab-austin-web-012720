a576fc0149230bc2cf19449688ceb9a7d311e16c
Rick Glascock <rixong@gmail.com>
2020-01-19 00:39:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,7 +25,7 @@ def reduce_to_all_true (source_array)
 source_array
 end
 
-def reduce_to_any_true (source_array)
-  source_array.reduce (){ |ele| ele }
+def reduce_to_any_true (source_array, starting_point)
+  source_array.reduce (starting_point){ |sum, ele| sum + ele }
 end
 
