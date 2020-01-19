2049c99a5ad6edbb4e9aea223b1c49296fea24ed
Rick Glascock <rixong@gmail.com>
2020-01-19 00:33:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,7 +6,7 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
- source_array.map { |ele| ele ) }
+ source_array.map { |ele| ele }
 end
 
 def map_to_double(source_array)
